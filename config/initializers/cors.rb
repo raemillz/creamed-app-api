a9f21ff1204833d4179e074a7753d1cd87d56bae
Rachel Miller <raeanne1991gmail.com@Rachels-MacBook-Pro.local>
2018-07-04 11:20:32 -0400

    connecting to client

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ['localhost:3000', 'https://rachelannemiller.com', 'https://rachelannemiller.com/creamed-app-client']
+    origins ['localhost:3000', 'http://rachelannemiller.com', 'http://rachelannemiller.com/creamed-app-client']
 
     resource '*',
       headers: :any,
